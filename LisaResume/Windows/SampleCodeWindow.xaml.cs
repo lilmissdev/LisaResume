5e24444534ecca22c86626ed5f2e090dfeb4cb8a
Jon Martin <jmartin@steelcloud.com>
2018-12-05 15:17:47 -0500

    Set the copy to output directory of all the components to true if newer

--- LisaResume/Windows/SampleCodeWindow.xaml.cs
+++ LisaResume/Windows/SampleCodeWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
 using System.Windows;
 
 using LisaResume.Back_End_Code.Classes;
@@ -14,16 +13,10 @@ namespace LisaResume.Windows
     public partial class SampleCodeWindow : Window
     {
         private Singleton singleton = Singleton.GetSingleton();
-        [DllImport("user32.dll")]
-        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
 
         private Process pDocked;
         private IntPtr hWndOriginalParent;
         private IntPtr hWndDocked;
-        public System.Windows.Forms.Panel panel;
         public string FilePath { get; set; }
         public SampleCodeWindow()
         {
